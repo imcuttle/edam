d453f8108479d822c8fdd8632134db0b7e8c348a
Cuttle Cong <moyuyc95@gmail.com>
2018-03-25 21:46:43 +0800

    chore: add ejs

--- packages/edam/src/core/Compiler/loaders/ejs.ts
+++ packages/edam/src/core/Compiler/loaders/ejs.ts
@@ -7,8 +7,12 @@
 
 import { template } from 'lodash'
 
+/** Used to match template delimiters. */
+let reEscape = /<%-([\s\S]+?)%>/g
+let reEvaluate = /<%([\s\S]+?)%>/g
+let reInterpolate = /<%=([\s\S]+?)%>/g
 
-function ejsLoader (content: string, variables: object): string {
+function ejsLoader(content: string, variables: object): string {
   return template(content, {})(variables)
 }
 
