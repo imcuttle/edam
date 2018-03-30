ff2cc90c71cfa1559cb2cba54286df722ef41db3
Cuttle Cong <moyuyc95@gmail.com>
2018-03-30 11:50:28 +0800

    chore: dulcet-prompt remove co

--- packages/edam-plugin-dulcet-prompt/index.js
+++ packages/edam-plugin-dulcet-prompt/index.js
@@ -5,15 +5,14 @@
  * @description
  */
 
-const co = require('co')
 const inquirer = require('./dulcet-inquirer')
 
 /* eslint-disable */
-module.exports = co.wrap(function* dulcetPrompt({ port, silent } = {}) {
+module.exports = function dulcetPrompt({ port, silent } = {}) {
   const edam = this
 
   edam.prompt = function(prompts, { context, yes } = {}) {
     return inquirer(prompts, { yes })
   }
 
-})
+}
